e322b8f7bb218765b5e08d900ffbbb4e0164d385
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2024-08-13 10:26:52 +0200

    10:26

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
-'''The minimum operations coding challenge.
-'''
+"""" 
+In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
 
-
+"""
 def minOperations(n):
-    '''Computes the fewest number of operations needed to result
-    in exactly n H characters.
-    '''
-    if not isinstance(n, int):
+    """ Prototype: def minOperations(n)
+Returns an integer
+If n is impossible to achieve, return 0
+Example:
+
+n = 9
+
+H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+
+Number of operations: 6 """
+    if not isinstance(n,int):
         return 0
-    ops_count = 0
-    clipboard = 0
-    done = 1
-    # print('H', end='')
-    while done < n:
-        if clipboard == 0:
-            # init (the first copy all and paste)
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif n - done > 0 and (n - done) % done == 0:
-            # copy all and paste
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif clipboard > 0:
-            # paste
-            done += clipboard
-            ops_count += 1
-            # print('-(01)->{}'.format('H' * done), end='')
-    # print('')
+    ops_count=0
+    clipboard=1
+    current=0
+    while clipboard < n:
+        if clipboard % 2 == 0:
+            ops_count +=2
+            clipboard += clipboard/2
+            print('2.{}'.format(ops_count))
+        else:
+            ops_count +=1
+            clipboard += clipboard
+            print('1.{}'.format(ops_count))
     return ops_count
