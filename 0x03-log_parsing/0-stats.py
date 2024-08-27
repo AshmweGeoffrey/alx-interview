f533ed532f481be6e4d0803431fc2baabd56efcd
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2024-08-27 15:37:56 +0200

    15:37

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,42 @@
 #!/usr/bin/python3
+""" Log parsing Script
+"""
 
-""" script that reads stdin line by line and computes metrics: """
 
 import sys
 
-def status_check(line):
-    splited_line=line.split()
-    try:
-        result={"code":splited_line[-2],"file_size":int(splited_line[-1])}
-    except IndexError:
-        pass
-    return result
-def print_metrics():
-    print("File size: {}".format(file_size))
-    for key,data in sorted(status.items()):
-        if data !=0:
-            print("{}: {}".format(key,data))
-status = {"200": 0, "301": 0, "400": 0, "401": 0,
+
+def printx(data, status):
+    """ print the log """
+    print("File size: {}".format(data))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
+status = {
+    "200": 0, "301": 0, "400": 0, "401": 0,
     "403": 0, "404": 0, "405": 0, "500": 0}
-file_size=0
-count=0
+counter = 0
+data = 0
 try:
     for line in sys.stdin:
-        if count == 10:
-            print_metrics()
-            count=1
+        if counter == 10:
+            printx(data, status)
+            counter = 1
         else:
-            count +=1
-        obtained_result=status_check(line)
-        if obtained_result != None:
-            try:
-                for key, value in status.items():
-                    if key == obtained_result["code"]:
-                        status[key] += 1
-            except Exception as e:
-                pass
-            try:
-                file_size += obtained_result["file_size"]
-            except Exception as e:
-                pass
-    print_metrics()
+            counter = counter + 1
+        parsed = line.split()
+        try:
+            data = data + int(parsed[-1])
+        except Exception as e:
+            pass
+        try:
+            for key, value in status.items():
+                if key == parsed[-2]:
+                    status[key] = status[key] + 1
+        except Exception as e:
+            pass
+    printx(data, status)
 except KeyboardInterrupt as e:
-    print_metrics()
+    printx(data, status)
